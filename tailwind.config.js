1a8d0e85b89827e96364ed6374940e51f5c02d11
hicham <hichamaraid10@gmail.com>
2022-03-11 17:35:32 +0100

    dx

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {},
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
