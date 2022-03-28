0c777ca0f3fa59549230e09f820a0a63eba52735
hicham <hichamaraid10@gmail.com>
2022-03-28 18:23:22 +0200

    xd

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="http://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ asset('/js/app.js') }}" defer></script>
     @inertiaHead
